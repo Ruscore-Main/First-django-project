5b17ebac3086e0921be69397df352728ac006775
Ruscore-Main <oobitovich@mail.ru>
2021-02-02 20:04:26 +0300

    added bootsrap

--- news/admin.py
+++ news/admin.py
@@ -3,10 +3,16 @@ from .models import *
 
 
 class NewsAdmin (admin.ModelAdmin):
-    list_display = ('id', 'title', 'created_at', 'updated_at', 'is_published')
+    list_display = ('id', 'title', 'category', 'created_at', 'updated_at', 'is_published')
     list_display_links = ('id', 'title')
-    search_fields = ('title', 'content')
+    search_fields = ('title','category', 'content')
+    list_editable = ('is_published',)
+    list_filter = ('is_published', 'category')
     
-
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    list_display_links = ('id', 'title')
+    search_fields = ('title',)
 
 admin.site.register(News, NewsAdmin)
+admin.site.register(Categories, CategoriesAdmin)
--- news/models.py
+++ news/models.py
@@ -3,15 +3,29 @@ from django.db import models
 class News(models.Model):
     title = models.CharField(max_length = 150, verbose_name = 'Наименование')
     content = models.TextField(blank=True, verbose_name = 'Контент')
-    created_at = models.DateTimeField(auto_now_add = True, verbose_name = 'Опубликовано')
+    created_at = models.DateTimeField(auto_now_add = True, verbose_name = 'Дата публикации')
     updated_at = models.DateTimeField(auto_now = True, verbose_name = 'Обновлено')
     photo = models.ImageField(upload_to = 'photos/%Y/%m/%d/', blank=True, verbose_name = 'Фото')
     is_published = models.BooleanField(default=True, verbose_name = 'Опубликовано?')
+    category = models.ForeignKey('Categories', on_delete = models.PROTECT, null = True, verbose_name = "Название категории")
 
     def __str__(self):
         return self.title
     
     class Meta:
         verbose_name = 'Новость'
-        verbose_name_plural = 'Нвости'
+        verbose_name_plural = 'Новости'
         ordering = ['-created_at', 'title']
+
+
+class Categories(models.Model):
+    title = models.CharField(max_length = 150, verbose_name = "Название категории", db_index = True)
+    
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
